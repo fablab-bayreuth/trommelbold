e1c3f96b27d89e8a32d055833833dfb7ce5b332d
messlinger <stephan.messlinger@uni-bayreuth.de>
2016-04-28 06:05:45 +0200

    Merged via_midi_2 to via_midi. The user can now switch between the two software serial implementations at compile time.

--- firmware/README.md
+++ firmware/README.md
@@ -28,9 +28,13 @@ Control the drum channels via the MIDI interface. Channels 1-8 are triggered
 by MIDI notes 60,62,64,65,67,69,71,71 (one octave in the middle of my MIDI keyboard,
 white keys only). Solenoid on time is controlled by the key velocity.
 
-**NOTE**: At the moment, this firmware uses the Arduino's SoftwareSerial together with
-the external Arduino Midi library (http://playground.arduino.cc/Main/MIDILibrary),
-which needs to be installed separately. Both dependencies will probably be dropped in
-future versions. (SoftwareSerial completely blocks the system while sending and receiving.)
+**NOTE**: 
+In the current version, the user can switch between the built-in software serial 
+class from the Arduino library and out own, more reliably software serial 
+implementation. (The built-in version blocks the system while sending and receiving.)
+This firmware uses the external Arduino Midi library 
+(http://playground.arduino.cc/Main/MIDILibrary),
+which needs to be installed separately. This dependency might be dropped in future
+versions.
 
 
--- firmware/via_midi/via_midi.ino
+++ firmware/via_midi/via_midi.ino
@@ -1,37 +1,56 @@
 
-
 /*
  * Control drums via MIDI.
  */
 
+// By default, we use our own software serial implementation for midi i/o, since the version
+// from the Arduino library has some severe deficiencies (eg. completly blocks while sending
+// and receiving.
+#define  USE_ARDUINO_SOFT_SERIAL  1  // set to 1 to use the built-in arduino software serial
+
 //=============================================================================================================
+// Midi stuff
 
-#include <SoftwareSerial.h>
-SoftwareSerial soft_serial(2,3);
+#include <MIDI.h>  // Arduino midi library (not a standard library, must be installed
 
-#include <MIDI.h>
-MIDI_CREATE_INSTANCE(SoftwareSerial, soft_serial, MIDI);  // do not use midi (small letters) as object name
-                                                          //   (is already used as namespace name)
+#if USE_ARDUINO_SOFT_SERIAL
+    #include <SoftwareSerial.h>
+    SoftwareSerial soft_serial(2,3);
+    MIDI_CREATE_INSTANCE(SoftwareSerial, soft_serial, MIDI);
+#else
+    #include "midi_serial.h"
+    MidiSoftwareSerial  midi_soft_serial;
+    MIDI_CREATE_INSTANCE(MidiSoftwareSerial, midi_soft_serial, MIDI);
+    // :NOTE: We can not use midi (small letters) as name for the midi-object,
+    //   since the library already uses this name for the namespace
+#endif
+
+//=============================================================================================================
+// Trommelbold
+
 #include "trommelbold.h"
 Trommelbold drum;
-
-
-//=============================================================================================================
-
 
 //=============================================================================================================
 void setup() 
 {
-  // :NOTE: Set the hardware serial baudrate SMALLER than the software serial baudrate (31250 for midi)  
-  Serial.begin(19200);  
-
-  // Listen to MIDI on all channels
-  MIDI.begin(MIDI_CHANNEL_OMNI);
-
-  // :NOTE: Per default, the midi class copies everything to modi out. This will go horribly wrong with
-  //   SoftwareSerial interface whose write function is BLOCKING! 
-  // Turn Thru off AFTER calling begin (will be re-activated by begin). 
-  MIDI.turnThruOff();    
+    // :NOTE: Hen using the built-in software serial class, it is import ant to set the hardware serial baudrate
+    //   SMALLER than the software serial baudrate (31250 for midi). When using our custom software serial 
+    //   implementation, this is no issue.
+    Serial.begin(19200);  
+    
+    // Listen to MIDI on all channels
+    MIDI.begin(MIDI_CHANNEL_OMNI);
+    
+    // :NOTE: Per default, the midi class resends every received event to the midi out port immediately 
+    //  (software "midi through" function).  With the built-in software serial class, this will go horribly
+    //  wrong, since it completely blocks during send, and therefore misses further input events.
+    #if  USE_ARDUINO_SOFT_SERIAL
+        // :NOTE: Turn Thru function off AFTER calling MIDI.begin (will be re-activated by MIDI.begin). 
+        MIDI.turnThruOff();
+    #else
+        MIDI.turnThruOn();
+    #endif
 }
 
 //=============================================================================================================
@@ -45,44 +64,44 @@ void loop() {
 
 void midi_tick()
 {
-  uint8_t type = 0, note = 0, vel = 0;
-  int8_t chan = -1;
-  if (MIDI.read())
-  {
-      type = MIDI.getType(); 
-      switch(type)      
-      {
-          case midi::NoteOn:
-          case midi::NoteOff:
-              note = MIDI.getData1();
-              vel =  MIDI.getData2();
-              print_midi(type, note, vel);
-              chan = midi_note_to_drum(note);
-              if (chan>=0) {
-                if (type == midi::NoteOn)  
-                     drum.hit( chan, vel );
-                else drum.release( chan );
-              }
-          break;
-       }
-  }
+    uint8_t stat = 0, note = 0, vel = 0;
+    int8_t chan = -1;
+    if (MIDI.read())
+    {
+        stat = MIDI.getType(); 
+        switch(stat)      
+        {
+            case midi::NoteOn:
+            case midi::NoteOff:
+                note = MIDI.getData1();
+                vel =  MIDI.getData2();
+                print_midi(stat, note, vel);
+                chan = midi_note_to_drum(note);
+                if (chan>=0) {
+                    if (stat == midi::NoteOn)  
+                         drum.hit( chan, vel );
+                    else drum.release( chan );
+                }
+            break;
+        }
+    }
 }
 
 
 int8_t midi_note_to_drum(uint8_t note)
 {
-  switch (note)
-  {
-    case 60: return 0;  // = C on middle octave
-    case 62: return 1;
-    case 64: return 2;
-    case 65: return 3;
-    case 67: return 4;
-    case 69: return 5;
-    case 71: return 6;
-    case 72: return 7;
-  }
-  return -1;
+    switch (note)
+    {
+        case 60: return 0;  // = C on middle octave
+        case 62: return 1;
+        case 64: return 2;
+        case 65: return 3;
+        case 67: return 4;
+        case 69: return 5;
+        case 71: return 6;
+        case 72: return 7;
+    }
+    return -1;
 }
 
     
@@ -90,22 +109,9 @@ int8_t midi_note_to_drum(uint8_t note)
 
 void print_midi(uint8_t stat, uint8_t data1, uint8_t data2)
 {
-  Serial.print("MIDI: ");
-  Serial.print(to_hex((stat>>4)&0x0f));
-  Serial.print(to_hex(stat&0x0f));
-  Serial.print(" ");
-  Serial.print(to_hex((data1>>4)&0x0f));
-  Serial.print(to_hex(data1&0x0f));
-  Serial.print(" ");
-  Serial.print(to_hex((data2>>4)&0x0f));
-  Serial.print(to_hex(data1&0x0f));
-  Serial.println("");
+    Serial.print("MIDI: "); Serial.print(stat, HEX);
+    Serial.print(" "); Serial.print(data1, HEX);
+    Serial.print(" "); Serial.println(data2, HEX);
 }
 
-char to_hex(uint8_t in)
-{
-  if (in>16) return ' ';
-  if (in<10) return '0'+in;
-  else return 'A'+in-10;
-}
 
